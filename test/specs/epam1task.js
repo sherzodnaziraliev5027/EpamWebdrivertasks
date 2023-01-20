d34987d30fb0056c7a8371fde78cab55120403ab
Sherzod Naziraliev <109876816+sherzodnaziraliev5027@users.noreply.github.com>
2023-01-20 23:11:18 +0500

    Update epam1task.js

--- test/specs/epam1task.js
+++ test/specs/epam1task.js
@@ -5,14 +5,10 @@ describe("Epam task 1",() => {
     // Task 1
     it("Task 1",async() => {
         await homePage.open();
-        
+        await homePage.textArea.waitForExist({timeout:3000})
         await homePage.textArea.setValue("Hello from WebDriver");
-        
         await homePage.pasteNameTitle.setValue("helloweb");
-        
         await homePage.makingVisiblePasteExpiration.click();
         await homePage.selectingPasteExpiration.click();
-
-        await browser.pause(5000);
     })
 })
